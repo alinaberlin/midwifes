fcbff74d2d5faaeb0359542dc370e7d1123b6bc9
Alina Ghetler <alinaghetler@Alinas-MacBook-Pro.local>
2018-10-28 15:11:28 +0100

    refactoring callback, replace callback with cb

--- services/dbservice.js
+++ services/dbservice.js
@@ -4,16 +4,10 @@ const fs = require('fs');
 module.exports = {
     save(data,cb) {
         //console.log(data) 
-         fs.writeFile('data.json', JSON.stringify(data), function(err){
-            console.log('save succesfull')
-            cb(err)
-        })
+         fs.writeFile('data.json', JSON.stringify(data), cb)
     },
     load(cb) {
-       fs.readFile('data.json', function(err, data) {
-           console.log('load successfull')
-           cb(err, data)
-       }) 
+       fs.readFile('data.json', cb)
     }
 }
 
