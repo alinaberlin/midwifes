f68010aa0549475ef2041768f2ef590f0e8c4857
Alina Ghetler <alinaghetler@Alinas-MacBook-Pro.local>
2018-10-28 23:05:25 +0100

    promises

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ const Database = require('./services/dbservice')
 const midwife = new Midwife('Alina', 'Landsberger Allee 133', 33, ['Eng', 'De', 'Ro']);
 const midwife2 = new Midwife('Maria', 'Landsberger Allee 153', 33, ['Eng', 'De']);
 
-console.log(midwife)
+//console.log(midwife)
 
 const patient = new Patient('Cosmina', 'Edison Square 9', 18, ['Eng', 'De', 'Cz']);
-console.log(patient)
+//console.log(patient)
 
 
 const rating = new Rating(5, 'Bad', patient)
-console.log(rating)
+//console.log(rating)
 midwife.addRating(rating)
-console.log('midwife rating', midwife.calculateScore())
+//console.log('midwife rating', midwife.calculateScore())
 
-const callback = (err) => {
-    if (!err) {
-        Database.load(function (err, data) {
-            console.log(JSON.stringify(data))
-        }
-        )
-    }
-}
 
-Database.save([midwife, midwife2], callback)
+
+Database.save([midwife, midwife2])
+.then(resp => console.log(resp))
+.catch(err => console.log(err))
+
+Database.load()
+.then(resp =>console.log(resp))
+.catch(err => console.log(err))
+
 
 
 /*Database.save([midwife, midwife2])
--- services/dbservice.js
+++ services/dbservice.js
@@ -2,12 +2,26 @@
 const fs = require('fs');
 
 module.exports = {
-    save(data,cb) {
+    save(data) {
         //console.log(data) 
-         fs.writeFile('data.json', JSON.stringify(data), cb)
+        // fs.writeFile('data.json', JSON.stringify(data), cb)
+        return new Promise ((resolve, reject) =>{
+            fs.writeFile('data.json', JSON.stringify(data), err => {
+                if(err) return reject (err);
+                resolve(true);
+            }) 
+
+         })
+
     },
-    load(cb) {
-       fs.readFile('data.json', cb)
+    load() {
+      return new Promise((resolve, reject) => { 
+          fs.readFile('data.json', (err, contents) => {
+              if(err) return reject(err);
+              resolve (JSON.parse(contents));
+          }) 
+        })
     }
 }
+
 
